8d687b58648d51058d128721fde92f1a60b890ec
anislerouge <anislerouge@gmail.com>
2025-03-13 22:06:07 +0100

    Setup GitHub Pages deployment

--- nationalite-entreprise-viz/vite.config.ts
+++ nationalite-entreprise-viz/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/nationalite-entreprenariat-data/', // Base path for GitHub Pages
 })
